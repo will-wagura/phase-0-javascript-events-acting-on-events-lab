79025940e5f6a43cd33876fa569c4b91a6e88007
William Ndirangu <wwagura0@gmail.com>
2024-03-17 22:49:33 +0300

    Moves

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function moveDodgerLeft() {
+    const left = parseInt(dodger.style.left);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const left = parseInt(dodger.style.left);
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
